852b0bf1de687e8845f68b5f59e5b328953a332b
Moyung <sdh240201@sdh.hs.kr>
2025-12-19 15:20:21 +0900

    AAA

--- Assets/moyung/Script/Butten.cs
+++ Assets/moyung/Script/Butten.cs
@@ -3,13 +3,12 @@ using UnityEngine.SceneManagement;
 
 public class Butten : MonoBehaviour
 {
+    int j = 0;
+    private int[] cameraIdx = new int[4];
+
     public void StartGameScene()
     {
         SceneManager.LoadScene(1);
     }
 
-    public void TestButten()
-    {
-        SceneManager.LoadScene(0);
-    }
 }
---
+++ Assets/moyung/Script/MoveCamera.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    [SerializeField] private CinemachineCamera[] cameras;
+    [SerializeField] FadeEffect fadeEffect;
+
+    private int cameraIdx = 0;
+    private bool isDelay;
+
+    void Start()
+    {
+        SetCamera(cameraIdx);
+        isDelay = false;
+    }
+
+    public void CameraMoveLeft()
+    {
+        if (isDelay == true) return;
+
+        fadeEffect.OnFade(FadeState.FadeInOut);
+
+        cameraIdx = (cameraIdx + 1) % cameras.Length;
+        SetCamera(cameraIdx);
+
+        StartCoroutine(Delay(1f));
+    }
+
+    public void CameraMoveRight()
+    {
+        if (isDelay == true) return;
+
+        fadeEffect.OnFade(FadeState.FadeInOut);
+
+        cameraIdx = (cameraIdx - 1 + cameras.Length) % cameras.Length;
+        SetCamera(cameraIdx);
+
+        StartCoroutine(Delay(1f));
+    }
+
+    private void SetCamera(int index)
+    {
+        for (int i = 0; i < cameras.Length; i++)
+        {
+            cameras[i].Priority = (i == index) ? 10 : 0;
+        }
+    }
+
+    private IEnumerator Delay(float time)
+    {
+        isDelay = true;
+        yield return new WaitForSeconds(time);
+        isDelay = false;
+    }
+}

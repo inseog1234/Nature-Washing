57c2f016685ad6b1f967537d810e0255ab17969c
Moyung <sdh240201@sdh.hs.kr>
2025-12-22 08:32:34 +0900

    카메라 이동

---
+++ Assets/moyung/Script/FollowCursor.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class FollowCursor : MonoBehaviour
+{
+    [SerializeField] Texture2D cursorImg;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Cursor.SetCursor(cursorImg, Vector2.zero, CursorMode.Auto);
+    }
+
+    
+}
